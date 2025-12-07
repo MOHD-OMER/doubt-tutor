35585ce0d4bca47a977135e0b0daf1bec7ea5138
Mohd Omer <mohammedabdulomer99@gmail.com>
2025-12-07 15:46:18 +0530

    Updated UI

--- ui/pages/2_How_It_Works.py
+++ ui/pages/2_How_It_Works.py
@@ -2,6 +2,14 @@
 import streamlit as st
 from pathlib import Path
 
+import sys
+from pathlib import Path
+
+# Add project root to PYTHONPATH so "src" and "ui" imports work
+ROOT_DIR = Path(__file__).resolve().parents[2]
+sys.path.insert(0, str(ROOT_DIR))
+
+
 # --------------------------------------------------
 # Page Config
 # --------------------------------------------------
